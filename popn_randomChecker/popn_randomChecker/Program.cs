cfb6b7da0408650f15d9b00923fdce82fa6901b0
u6(縞々) <shimada2000@gmail.com>
2016-10-28 10:10:27 +0900

    Update Program.cs

--- popn_randomChecker/popn_randomChecker/Program.cs
+++ popn_randomChecker/popn_randomChecker/Program.cs
@@ -8,14 +8,18 @@ namespace popn_randomChecker
     {
         static void Main(string[] args)
         {
+            //args[0]で入力されたファイルのテキストを読み取ってこんばーと
             var cvt = new ConvertTextClass();
             var pat = cvt.ConvertToList(args[0]);
 
+            //乱のすべてのパターンを試してそれぞれの無理押しの回数をカウントする
             var ranche = new RandomChecker();
             var res = ranche.checkMurioshiCount(pat);
 
+            //無理押し回数の少ない順にオーダー
             var wei = res.OrderBy(r => r.Patterns.Count).ToList();
 
+            //args[1]にそれを出力する
             System.IO.StreamWriter sw = new System.IO.StreamWriter(
             args[1],
             false,
